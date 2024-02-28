ca0311892ae46d3fd1d5570089c570af9ded1dc6
Gustavo Soré <gustavosore@MacBook-Air-de-Gustavo.local>
2024-02-28 20:32:32 -0300

    Improve Neural Network Main

--- main.c
+++ main.c
@@ -8,19 +8,37 @@
 
 int main(int argc, char *argv[]) {
 
+    printf("NeuralNetwork - Kaggle - MNIST in CSV\nhttps://www.kaggle.com/datasets/oddrationale/mnist-in-csv\n");
+    printf("1 - Traing\n");
+    printf("2 - Predict\n");
+    printf("Any Other - Exit\n");
+    int option = 0;
+    scanf("%d", &option);
     srand(time(NULL));
-
-    // int numberOfImages = 59000;
-	// Image** images = readImagesFromFile("./data/mnist_train.csv", numberOfImages);
-	// NeuralNetwork* network = createNeuralNetwork(784, 300, 10, 0.1);
-	// trainWithImages(network, images, numberOfImages);
-	// saveNeuralNetwork(network, "testing_net");
-
-    int numberOfImages = 3000;
-	Image** images = readImagesFromFile("data/mnist_test.csv", numberOfImages);
-	NeuralNetwork* network = loadNeuralNetwork("testing_net");
-	double score = predictOnImages(network, images, numberOfImages);
-	printf("Score: %1.5f\n", score);
+    NeuralNetwork* network;
+    Image** images;
+    printf("How mane images? (Max 60000)\n");
+    int numberOfImages = 0;
+    scanf("%d", &numberOfImages);
+    if (numberOfImages > 60000) numberOfImages = 6000;
+    switch (option)
+    {
+    case 1:
+        images = readImagesFromFile("./data/mnist_train.csv", numberOfImages);
+        network = createNeuralNetwork(784, 300, 10, 0.1);
+        trainWithImages(network, images, numberOfImages);
+        saveNeuralNetwork(network, "testing_net");
+        break;
+    case 2:
+        images = readImagesFromFile("data/mnist_test.csv", numberOfImages);
+        network = loadNeuralNetwork("testing_net");
+        double score = predictOnImages(network, images, numberOfImages);
+        printf("Score: %1.5f\n", score);
+        break;
+    default:
+        break;
+        exit(0);
+    }
 
     freeImages(images, numberOfImages);
 	freeNeuralNetwork(network);
